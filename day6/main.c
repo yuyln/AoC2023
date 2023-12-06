3d0a878923161b5a05abab99b4ef4a07d54cc690
yuyin <joseph.cbs.jabuticabal@gmail.com>
2023-12-06 18:45:39 -0300

    removed

--- day6/main.c
+++ day6/main.c
@@ -5,17 +5,13 @@
 #include <inttypes.h>
 #include <assert.h>
 
-uint64_t traveled_given(uint64_t hold, uint64_t total_time) {
-    return hold * (total_time - hold);
-}
-
 uint64_t possible_ways(uint64_t total_time, uint64_t distance) {
-    //d(t, T) = t*(T-t)=tT-t^2 => tT-t^2-d=0
+    //d(t, T) = t*(T-t)=tT-t^2 => tT-t^2-d=0=>t^2-tT+d=0
     double d = distance + 0.1; //sum delta to avoid roots equal to the distance to break
     double T = total_time;
-    double x0 = (-T + sqrt(T * T - 4.0 * d)) / -2.0;
-    double x1 = (-T - sqrt(T * T - 4.0 * d)) / -2.0;
-    assert(x0 >= 0 && x1 >= 0 && x1 - x0 >= 0);
+    double x0 = (T - sqrt(T * T - 4.0 * d)) / 2.0;
+    double x1 = (T + sqrt(T * T - 4.0 * d)) / 2.0;
+    assert(x0 >= 0 && x1 >= 0 && x1 >= x0);
     uint64_t start = ceil(x0);
     uint64_t end = floor(x1);
     return end - start + 1;
